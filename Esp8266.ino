26248785dadd65a39cc6fc9cb19eed9f70e42d98
Christian.Al.Lo.Sa <47773256+Ghost68b@users.noreply.github.com>
2021-08-09 23:24:21 -0500

    Update Esp8266.ino

--- Esp8266.ino
+++ Esp8266.ino
@@ -3,6 +3,7 @@
 /*Pasos dentro del id de arduino*/
 /* Archivos/ preferencia // pegar la url en gestor de urls adicionales "https://arduino.esp8266.com/stable/package_esp8266com_index.json" */
 /* Herramientas/Gestor de tarjetas // descargar esp8266 e instalarlo   */
+/*Descargar las librerias de blynk e instalarlas en arduino en Programa icluir librerias las descomprimes y las instalas 1 x 1*/
 /*Herramientas y getor de placas y selecciona la placa  (NodeMCU 1.0) tambien el puerto en donde se encuentra instalada*/
 /*Recomendacion instala el id de arduino y no descarges el portable ya que tendras problemas al cargar el codigo a la placa*/
 
